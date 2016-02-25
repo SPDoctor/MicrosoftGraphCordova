950c4d6181d587badc93aa3c6739ea90416c0da7
SPDoctor <BillA@flosim.com>
2016-02-25 18:19:11 +0000

    Initial commit

---
+++ MicrosoftGraphCordova/plugins/cordova-plugin-ms-adal/CHANGELOG.md
@@ -0,0 +1,22 @@
+0.6.2 - February 18, 2016
+
+* windows: fixed Windows Phone 8.1 arm build architecture compatibility issue
+
+0.6.1 - February 12, 2016
+
+* ios: fixed #16 The file `ADALiOS.entitlements` couldn’t be opened
+* ios: fixed #35 `acquireTokenSilentAsync` fails to return a token after a successful login
+* ios: node version 5.6.0 support
+
+0.6.0 - June 10, 2015
+
+* ios: correctly support projects with a space in their name
+* android: ant support
+* Add Android permissions for broker.
+* windows: `sso` support enhancements and bug fixes
+* windows: Windows10 support and the latest native libs
+* fixes #9 Failure to Load Resource (utility.js.map)
+
+0.5.0 - April 28, 2015
+
+* Initial version
---
+++ MicrosoftGraphCordova/www/scripts/index.js
@@ -0,0 +1,122 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+	"use strict";
+	var tenantName = 'flosim';
+	var authority = "https://login.windows.net/" + tenantName + ".onmicrosoft.com";
+	var resourceUrl = 'https://graph.microsoft.com/';
+	var appId = "92f98787-c980-4c15-9be0-348ba4244408";
+	var redirectUrl = "http://localhost:4400/www/index.html";
+	var authContext;
+	var output;
+
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+	function onDeviceReady() {
+		// Handle the Cordova pause and resume events
+		document.addEventListener('pause', onPause.bind(this), false);
+		document.addEventListener('resume', onResume.bind(this), false);
+		document.getElementById("logout").addEventListener('click', onLogout.bind(this), false);
+		document.getElementById("loadToken").addEventListener('click', onLoadToken.bind(this), false);
+		output = document.getElementById("output");
+
+		// TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+	};
+
+	function fail(err) {
+		output.innerHTML = err.message;
+	}
+
+	function onLoadToken() {
+		getAccessToken(resourceUrl, appId, redirectUrl, function (response) {
+			var message = "";
+			message += "Access token: " + response.accessToken;
+			message += "<br />\r\n";
+			message += "Decoded token: " + decodeJWT(response.accessToken);
+			message += "<br />\r\n";
+			message += "Token will expire on: " + response.expiresOn;
+			message += "<br />\r\n";
+			output.innerHTML = message;
+
+			var endPointUri = "https://graph.microsoft.com/v1.0/";
+			var requestUri = endPointUri + 'me/contacts?$top=20';
+			var bearerToken = "Bearer " + response.accessToken;
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', requestUri);
+			xhr.setRequestHeader("Authorization", bearerToken);
+			xhr.setRequestHeader("Accept", "application/json;odata.metadata=minimal");
+			xhr.onload = function () {
+				if (xhr.status === 200) {
+					var response = JSON.parse(xhr.responseText);
+					var contacts = "";
+					for (var i = 0; i < response.value.length; i++) {
+						contacts += "<li>" + response.value[i].displayName + "</li>";
+					};
+					document.getElementById("contacts").innerHTML = contacts;
+				}
+				else {
+					output.innerHTML += '<br />Request failed.  Returned status of ' + xhr.status;
+				}
+			};
+			xhr.send();
+		}, fail);
+	};
+
+	function onLogout() {
+		if (authContext) authContext.tokenCache.clear();
+	}
+
+	function getAccessToken1(resourceUrl, appId, redirectUrl, success, fail) {
+		try {
+			// can try to use acquireTokenSilent here if already have a token
+			if (authContext.tokenCache.readItems()._value.length > 0) {
+				var tokenCacheItem = authContext.tokenCache.readItems()._value[0];
+				success(tokenCacheItem);
+				return;
+			}
+			authContext.acquireTokenAsync(resourceUrl, appId, redirectUrl).then(function (authResponse) {
+				success(authResponse);
+			}, fail);
+		} catch (ex) {
+			fail(ex.message);
+		}
+	}
+
+	function getAccessToken(resourceUrl, appId, redirectUrl, success, fail) {
+		try {
+			if (!authContext) {
+				Microsoft.ADAL.AuthenticationContext.createAsync(authority).then(function (context) {
+					authContext = context;
+					getAccessToken1(resourceUrl, appId, redirectUrl, success, fail);
+				}, fail);
+			}
+			else getAccessToken1(resourceUrl, appId, redirectUrl, success, fail);
+		} catch (ex) {
+			fail(ex.message);
+		}
+	}
+
+	function decodeJWT(encodedJWT) {
+		var decodedJWT = "";
+		try {
+			var sections = encodedJWT.split(".");
+			decodedJWT += "<br /><b>Header:</b> " + atob(sections[0]);
+			decodedJWT += "<br /><b>Payload:</b> " + atob(sections[1]);
+			decodedJWT += "<br /><b>Signature:</b> " + sections[2];
+			decodedJWT += "<br />";
+		} catch (ex) {
+			decodedJWT += "<br />Error: " + ex.message;
+		}
+		return decodedJWT;
+	}
+
+	function onPause() {
+		// TODO: This application has been suspended. Save application state here.
+	};
+
+	function onResume() {
+		// TODO: This application has been reactivated. Restore application state here.
+	};
+})();
